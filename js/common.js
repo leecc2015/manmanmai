5c199959bef724be9cecc4eaad202276f2316c3d
leecc2015 <583388040@qq.com>
2019-01-11 17:10:14 +0800

    首页静态页面完成

---
+++ js/common.js
@@ -0,0 +1,4 @@
+(window.onresize = window.__setFontSize__ = function () {
+    document.documentElement.style.fontSize = Math.min(750, Math.max(document.documentElement.clientWidth, 320)) / 375 * 37.5 + 'px'
+  }
+  )();
---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+
+// 功能1 渲染nav列表
+$.ajax({
+    type:'get',
+    url:'http://127.0.0.1:9090/api/getindexmenu',
+    dataType:'json',
+    success:function( data ){
+        var html = template('navTpl', data);
+        $('.mmm_nav').html(html);
+    }
+})
+
+$('.mmm_nav').on('click','a:nth-child(8)' ,function(e){
+    e.preventDefault();
+    $('.mmm_nav .other').toggleClass('hidden');
+})
+
+// 功能2 商品列表渲染
+$.ajax({
+    type:'get',
+    url:'http://127.0.0.1:9090/api/getmoneyctrl',
+    dataType:'json',
+    success:function( data ){
+        var html = template('goodListTpl', data);
+        $('.mmm_goodList ul').html(html);
+    }
+})
+
+
+
+
+
+
+
+})
